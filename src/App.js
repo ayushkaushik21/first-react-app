82d42c39e0935abdc14c4528717f3c7a663ef17d
ayush <ayush.kaushik@talentelgia.in>
2022-11-14 12:42:45 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Build From Jenkins
         </a>
       </header>
     </div>
