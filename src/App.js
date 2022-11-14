7a3209149c5faa4574e9cf08eb4b2ba60d23cf41
ayush <ayush.kaushik@talentelgia.in>
2022-11-14 12:55:44 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Build From Jenkins and React App and Node JS
+        Build
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code> src/App.js </code> and save to reload.
